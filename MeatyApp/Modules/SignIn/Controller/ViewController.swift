65b61550dcca8a5178f414f4e3cdfc5e327dd5d2
teach1997 <anhdk0103@gmail.com>
2019-07-21 00:31:28 +0700

    first commit

---
+++ MeatyApp/Modules/SignIn/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeatyApp
+//
+//  Created by Đào Kiều Anh on 7/17/19.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-MeatyApp/Pods-MeatyApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MeatyApp : NSObject
+@end
+@implementation PodsDummy_Pods_MeatyApp
+@end
